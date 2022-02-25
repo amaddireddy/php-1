07a0013ccc5a2f5ef3057f2a91a8d9ccc871663f
anitha <amaddireddy86@gmail.com>
2022-02-25 17:17:37 +0530

    dockerfile updated

--- docker-files/docker-script.sh
+++ docker-files/docker-script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo yum install docker -y
 sudo systemctl start docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
